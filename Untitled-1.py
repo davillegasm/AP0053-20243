5c15f347576762f3ff41eafaaff2ba12a174991c
davillegasm <davillegasm@udistrital.edu.co>
2024-08-14 14:30:32 -0500

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+print("mi primera vez con phyton")
+a= "cadena de caracteres"
+print("tipo de dato a:", type(a))
+b= 8
+print("tipo de dato b:", type(b))
+c= 3.1415
+print("tipo de dato c:", type(c))
